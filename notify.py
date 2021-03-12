d150127fdf81ad67e668c7d4bfd8f8ed3e9e2023
Ty-yang <1633549291@qq.com>
2021-03-12 14:36:40 +0800

    Update notify.py

--- notify.py
+++ notify.py
@@ -255,7 +255,7 @@ class Notify(object):
                 xiaoding = DingtalkChatbot(webhook, secret=secret)
                 msg = f'###{text}\n####{status}\n\n{desp}'
                 xiaoding.send_markdown(title=text, text=msg)
-#                 return true
+                return true
 #                 timestamp = int(round(time.time() * 1000))
 #                 secret_enc = bytes(secret).encode('utf-8')
 #                 string_to_sign = f'{timestamp}\n{secret}'
@@ -276,7 +276,7 @@ class Notify(object):
 #         conf = ['钉钉机器人', 'DD_BOT_TOKEN', DD_BOT_TOKEN, 'errcode', 0]
 #         name, needs, token, text, code  = conf
 
-        return self.pushTemplate('post', url, data=data, name=name, needs=needs, token=token, text=text, code=code)
+#         return self.pushTemplate('post', url, data=data, name=name, needs=needs, token=token, text=text, code=code)
 
     def wwBot(self, text, status, desp):
         WW_BOT_KEY = self.WW_BOT_KEY
